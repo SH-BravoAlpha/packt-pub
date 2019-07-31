e52eb051baad7dc95dcf5bf3f68fbb90c8a43fd2
Robert Arrington <rarrington@sincerelyhers.com>
2019-07-31 17:02:55 -0400

    [IMP] my_library - update relations and monetary fields

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.addons import decimal_precision as dp
 
 
 class LibraryBook(models.Model):
@@ -23,7 +24,25 @@ class LibraryBook(models.Model):
     date_updated = fields.Date('Last Updated')
     pages = fields.Integer('Number of Pages')
     reader_rating = fields.Float('Reader Average Rating', digits=(14, 4), )
+    cost_price = fields.Float('Book Cost', dp.get_precision('Book Price'))
+    currency_id = fields.Many2one('res.currency', string='Currency',
+                                  # optional: currency_field='currency_id',
+                                  )
+    retail_price = fields.Monetary('Retail Price', )
+    publisher_id = fields.Many2one('res.partner', string='Publisher',
+                                   # optional: ondelete='set null', context={}, domain=[],
+                                   )
 
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    published_book_ids = fields.One2many(
+        'library.book', 'publisher_id', string='Published Books')
+    authored_book_ids = fields.Many2many(
+        'library.book', string='Authored Books',
+        # relation='library_book_res_partner_rel' # optional,
+    )
+
+    #     @api.depends('value')
+    #     def _value_pc(self):
+    #         self.value2 = float(self.value) / 100
